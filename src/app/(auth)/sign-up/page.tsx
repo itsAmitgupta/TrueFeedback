5525f2273e45fd7b0e3777362c7a58f954e02674
Amit Gupta <guptaamitgupta819@gmail.com>
2024-07-30 11:18:59 +0530

    Otp verify frontend and signup frontend update

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -91,9 +91,9 @@ const page = () => {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-800">
-    <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+    <div className="w-full max-w-md p-4 space-y-8 bg-white rounded-lg shadow-md">
       <div className="text-center">
-        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-4">
           Join True Feedback
         </h1>
         <p className="mb-4">Sign up to start your anonymous adventure</p>
